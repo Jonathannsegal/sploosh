d1656dc1b993dbc08f1bfa59f714fb773f699a3b
ttsmith1 <47036293+ttsmith1@users.noreply.github.com>
2019-10-12 16:17:15 -0500

    f

---
+++ Assets/Splooosh/Game/movement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 mMinScale;
+    public float lifeTime = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        BuildingDie(collision.gameObject);
+    }
+
+    void BuildingDie(GameObject obj)
+    {
+        obj.gameObject.transform.Translate(Vector3.down);
+        Destroy(obj.gameObject, lifeTime);
+        BuildingDie(obj.gameObject);
+    }
+}
