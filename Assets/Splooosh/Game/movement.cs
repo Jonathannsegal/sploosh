309a3f1f90fad130093c0c55ad6083e999fc8885
Jonathan <jonathannsegal@gmail.com>
2019-10-12 20:22:04 -0500

    Tdog

--- Assets/Splooosh/Game/movement.cs
+++ Assets/Splooosh/Game/movement.cs
@@ -11,8 +11,6 @@ public class movement : MonoBehaviour
     public float moveSpeed = 10f;
     public float turnSpeed = 50f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
     }
@@ -30,17 +28,6 @@ public class movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.RightArrow))
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-    }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        BuildingDie(collision.gameObject);
-    }
-
-    void BuildingDie(GameObject obj)
-    {
-        obj.gameObject.transform.Translate(Vector3.down);
-        Destroy(obj.gameObject, lifeTime);
-        BuildingDie(obj.gameObject);
     }
 }
